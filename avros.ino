d1211714f1e0cbd7bfda4da39b52ffa0c19a1f80
saltydalty <asificam1@gmail.com>
2016-03-15 10:38:15 -0600

    Added files via upload

---
+++ avros.ino
@@ -0,0 +1,30 @@
+//
+//
+//Main AVR OS
+//created by Dalton Herrewynen on March 14 of 2016
+//Licensed under the GNU GPL v2.0
+//
+
+/*
+Designed for: Atmel ATMEGA328
+Boards used in construction: Arduino nano
+
+Check list:
+1. Scavenge parts for simple external program storage (audio recorder like a cassette, USB floppy, or USB flashdrive)
+2. develope a console function that can draw letters on a bitmap and send them to graphics AVR
+3. Scavenge a keyboard input solution (USB, or Serial)
+4. Code in the keyboard
+5. begin developing the OS
+6. Find a simple compiler for the OS
+7. install compiler and run test scripts.
+
+*/
+
+void setup(){
+  Serial.begin(115200);//maximum serial speed for graphics AVR
+  
+}
+
+void loop(){
+  //work will begin here
+}
