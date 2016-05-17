d53296647143b241bfc2fedc80508d7bc66445c7
saltydalty <asificam1@gmail.com>
2016-05-16 20:33:29 -0600

    Update avros.ino

--- avros.ino
+++ avros.ino
@@ -7,12 +7,12 @@
 
 /*
 Designed for: Atmel ATMEGA328
-Boards used in construction: Arduino nano
+Boards used in construction: Arduino nano (CPU), Arduino nano (GPU).
 
 Check list:
 1. Scavenge parts for simple external program storage (audio recorder like a cassette, USB floppy, or USB flashdrive)
 2. develope a console function that can draw letters on a bitmap and send them to graphics AVR
-3. Scavenge a keyboard input solution (USB, or Serial)
+3. Figure out a keyboard input solution (USB, or Serial)
 4. Code in the keyboard
 5. begin developing the OS
 6. Find a simple compiler for the OS
@@ -21,10 +21,16 @@ Check list:
 */
 
 void setup(){
-  Serial.begin(115200);//maximum serial speed for graphics AVR
+  Serial.begin(115200);//maximum serial speed for graphics AVR, serial bus is serving the same purpose a PCIe bus does in a full sized machine
   
 }
 
 void loop(){
-  //work will begin here
+  //work on the OS will begin here
+  
+}
+
+void append(String addition){//append string to console, let graphics sketch do the rest
+  Serial.writeln(addition);
+}
 }
